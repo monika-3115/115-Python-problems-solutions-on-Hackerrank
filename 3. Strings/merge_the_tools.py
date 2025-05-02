8db8dbd2d7a4873c736f7ea0b4c14bd0c8ee98a0
monika-3115 <moni115pallaprolu@gmail.com>
2025-05-03 00:22:27 +0530

    strings

---
+++ 3. Strings/merge_the_tools.py
@@ -0,0 +1,13 @@
+def merge_the_tools(string, k):
+    # your code goes here
+    for i in range(0, len(string), k):
+        subs = string[i:i+k]
+        final_str = ''
+        for ch in subs:
+            if ch not in final_str:
+                final_str += ch
+        print(final_str)
+
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
